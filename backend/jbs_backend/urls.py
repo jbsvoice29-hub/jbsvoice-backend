cf6937fc9fc85570d7ef001b54061f8d74a84a86
Santhosh Chodipilli <santhoshchodipilli@Santhoshs-MacBook-Air.local>
2025-11-22 16:39:21 +0530

    urls.py updated

--- backend/jbs_backend/urls.py
+++ backend/jbs_backend/urls.py
@@ -2,13 +2,15 @@
 URL configuration for jbs_backend project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apparel.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 # Serve media files in development
